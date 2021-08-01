1285ce4609712f5e186b8555bc7b67e17519b0fc
jihyun <kjwise1128@gmail.com>
2021-08-01 17:02:34 +0900

    pull

--- Front/components/Menu2/Subscription.jsx
+++ Front/components/Menu2/Subscription.jsx
@@ -8,7 +8,7 @@ const Subscription = () => {
             <div className="bank_site">
             <h4>방법2. 청약저축통장 가입은행 사이트 방문하여 개인로그인 후 납입횟수 조회</h4>
                 <ul>
-                    <li className="kb"><a href="https://okbfex.kbstar.com/quics?page=onhouse#loading"><img src="KB.png" /></a></li>
+                    <li className="kb"><a href="https://okbfex.kbstar.com/quics?page=onhouse#loading"><img src="/KB.png" /></a></li>
                     <li className="sh"><a href="https://mybank.ibk.co.kr/uib/jsp/index.jsp"><img src="/IBK.png" /></a></li>
                     <li className="nh"><a href="https://banking.nonghyup.com/servlet/content/ip/ph/IPPH0001M.thtml"><img src="/NH.png" /></a></li>
                     <li className="woori"><a href="https://svc.wooribank.com/svc/Dream?withyou=hb"><img src="/WOORI.png" /></a></li>
