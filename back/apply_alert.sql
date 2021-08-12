1839f21387ff485893eaf1b909e82fe2d5ae4b48
dfassf <dfassf@gmail.com>
2021-08-12 17:44:09 +0900

    D

--- back/apply_alert.sql
+++ back/apply_alert.sql
@@ -7,8 +7,10 @@ insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) va
 
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남복정1', 'GyeongGi', '성남시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 134호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남복정1', 'GyeongGi', 'not', 0, '지원 자격이 없습니다');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남복정1', 'not', 'not', 0, '지원 자격이 없습니다');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('의왕청계2', 'GyeongGi', '의왕시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 92호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('의왕청계2', 'GyeongGi', 'not', 0, '지원 자격이 없습니다');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('의왕청계2', 'not', 'not', 0, '지원 자격이 없습니다');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('위례', 'GyeongGi', '성남시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 127호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('위례', 'GyeongGi', 'not', 24, '경기도 거주자 이시네요! 1단계 공급물량은 총 89호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('위례', 'not', 'not', 0, '기타지역 거주자/미해당 이시네요! 1단계 공급물량은 총 63호 입니다.');
@@ -16,24 +18,31 @@ insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) va
 
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남낙생', 'GyeongGi', '성남시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 270호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남낙생', 'GyeongGi', 'not', 0, '지원 자격이 없습니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남낙생', 'not', 'not', 0, '지원 자격이 없습니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남복정2', 'GyeongGi', '성남시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 180호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남복정2', 'GyeongGi', 'not', 0, '지원 자격이 없습니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남복정2', 'not', 'not', 0, '지원 자격이 없습니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('군포대야미', 'GyeongGi', '군포시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 300호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('군포대야미', 'GyeongGi', 'not', 0, '지원 자격이 없습니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('군포대야미', 'not', 'not', 0, '지원 자격이 없습니다.');
 
 
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('의왕월암', 'GyeongGi', '의왕시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 240호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('의왕월암', 'GyeongGi', 'not', 0, '지원 자격이 없습니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('의왕월암', 'not', 'not', 0, '지원 자격이 없습니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('수원당수', 'GyeongGi', '수원시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 150호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('수원당수', 'GyeongGi', 'not', 24, '경기도 거주자 이시네요! 1단계 공급물량은 총 105호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('수원당수', 'not', 'not', 0, '기타지역 거주자/미해당 이시네요! 1단계 공급물량은 총 75호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('부천원종', 'GyeongGi', '부천시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 120호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('부천원종', 'GyeongGi', 'not', 0, '지원 자격이 없습니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('부천원종', 'not', 'not', 0, '지원 자격이 없습니다.');
 
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('시흥하중', 'GyeongGi', '시흥시', 12, '당해기준 충족자 이시네요! 1단계 공급물량은 총 210호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('시흥하중', 'GyeongGi', 'not', 0, '당해 미달시 지원 가능합니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('시흥하중', 'not', 'not', 0, '당해 미달시 지원 가능합니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('과천주암', 'GyeongGi', '과천시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 420호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('과천주암', 'GyeongGi', 'not', 0, '당해 미달시 지원 가능합니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('과천주암', 'not', 'not', 0, '당해 미달시 지원 가능합니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('남양주왕숙2', 'GyeongGi', '남양주시', 12, '당해기준 충족자 이시네요! 1단계 공급물량은 총 210호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('남양주왕숙2', 'GyeongGi', 'not', 6, '경기도 거주자 이시네요! 1단계 공급물량은 총 147호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('남양주왕숙2', 'not', 'not', 0, '기타지역 거주자/미해당 이시네요! 1단계 공급물량은 총 105호 입니다.');
@@ -68,3 +77,5 @@ insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) va
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('구리갈매역세권', 'not', 'not', 0, '기타지역 거주자/미해당 이시네요! 1단계 공급물량은 총 165호 입니다.');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남금토', 'GyeongGi', '성남시', 24, '당해기준 충족자 이시네요! 1단계 공급물량은 총 210호 입니다');
 insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남금토', 'GyeongGi', 'not', 0, '지원 자격이 없습니다.');
+insert into apply_alert1 (apply, Location, dbLocation, option2, apply_alert1) values ('성남금토', 'not', 'not', 0, '지원 자격이 없습니다.');
+
