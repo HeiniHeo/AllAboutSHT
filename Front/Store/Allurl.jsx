8c2bccc699d0bca80e88463197fce8b05dda634c
jihyun <kjwise1128@gmail.com>
2021-08-18 11:19:17 +0900

    pull 용 커밋

--- Front/Store/Allurl.jsx
+++ Front/Store/Allurl.jsx
@@ -1,3 +1,4 @@
 export const base_url = 'http://api.jebaldangcheom.com'
 export const clickurl = 'http://jebaldangcheom.com/faqs/'
 export const faqurl = 'http://api.jebaldangcheom.com/faqs/'
+export const localurl = 'http://localhost'
